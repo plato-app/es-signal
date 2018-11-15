a7eeb5d997df6837a00391b257950c74978d6d09
Geoff Blair <geoff@platoteam.com>
2018-11-15 11:22:39 -0700

    :white_check_mark: Add more tests

--- tests/index.js
+++ tests/index.js
@@ -1,27 +1,41 @@
 const tape = require("tape");
 const { Signal } = require("../dist");
 
-tape("smoke", (t) => {
-  t.plan(4);
+tape("receive", (t) => {
+	t.plan(2);
 
-  const sig = new Signal();
+	const sig = new Signal();
 
-  sig.receive((a, b) => {
-    t.equal(a, 10);
-    t.equal(b, 20);
-  });
+	sig.receive((a, b) => {
+		t.equal(a, 10);
+		t.equal(b, 20);
+	});
 
-  sig.receive((a, b) => {
-    t.equal(a, 10);
-    t.equal(b, 20);
-  });
+	sig.emit(10, 20);
+});
 
-  const rec = () => {
-    t.fail("This receiver should have been removed");
-  };
+tape("ignore", (t) => {
+	const sig = new Signal();
 
-  sig.receive(rec);
-  sig.ignore(rec);
+	const rec = () => {
+		t.fail("This receiver should have been ignored");
+	};
 
-  sig.emit(10, 20);
+	sig.receive(rec);
+	sig.ignore(rec);
+
+	sig.emit();
+	t.end();
+});
+
+tape("purge", (t) => {
+	const sig = new Signal();
+
+	sig.receive(() => {
+		t.fail("This receiver should have been purged");
+	});
+
+	sig.purge();
+	sig.emit();
+	t.end();
 });

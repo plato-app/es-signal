a7135b2c39191ddc64f985f2af5fe0bae6764e98
Geoff Blair <geoff@platoteam.com>
2019-05-25 18:52:01 -0600

    :fire: Remove dist folder from source control

--- dist/index.d.ts
+++
@@ -1,8 +0,0 @@
-export declare type SignalReceiver = (...args: any[]) => void;
-export declare class Signal<R extends SignalReceiver> {
-    private receivers;
-    receive(receiver: R): void;
-    ignore(receiver: R): void;
-    purge(): void;
-    emit(...args: Parameters<R>): void;
-}

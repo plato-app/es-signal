6d8e53a75fa107489abf1abd48572030eebfde45
Geoff Blair <geoff@mousepox.io>
2021-03-14 14:22:31 -0600

    Migrate from TSLint -> ESLint

--- lib/index.ts
+++ lib/index.ts
@@ -1,4 +1,5 @@
 /** Generic receiver callback */
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export type SignalReceiver = (...args: any[]) => void;
 
 /** Signal emitter */
@@ -16,7 +17,7 @@ export class Signal<R extends SignalReceiver = SignalReceiver> {
 	 * Attaches a receiver to this signal
 	 * @param receiver Signal receiving callback function
 	 */
-	public receive(receiver: R) {
+	public receive(receiver: R): void {
 		this.receivers.push(receiver);
 	}
 
@@ -24,7 +25,7 @@ export class Signal<R extends SignalReceiver = SignalReceiver> {
 	 * Detaches a reciever from this signal
 	 * @param receiver Signal receiving callback function
 	 */
-	public ignore(receiver: R) {
+	public ignore(receiver: R): void {
 		const index = this.receivers.indexOf(receiver);
 		if (index !== -1) {
 			this.receivers.splice(index, 1);
@@ -32,7 +33,7 @@ export class Signal<R extends SignalReceiver = SignalReceiver> {
 	}
 
 	/** Detaches ALL receivers from this signal */
-	public purge() {
+	public purge(): void {
 		this.receivers.length = 0;
 	}
 
@@ -40,7 +41,7 @@ export class Signal<R extends SignalReceiver = SignalReceiver> {
 	 * Emits this signal by notifying all receivers
 	 * @param args Arguments passed to receivers of this signal
 	 */
-	public emit(...args: Parameters<R>) {
+	public emit(...args: Parameters<R>): void {
 		for (const receiver of this.receivers) {
 			receiver(...args);
 		}
